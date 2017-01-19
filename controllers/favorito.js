7f63bfd73ef4d3f0e5f8d53bdbf8b5018e392377
Oscar R.C <oscarrc.web@gmail.com>
2017-01-19 18:54:56 +0100

    Instalación MongoDB y Mongoose
    Se ha instalado mongodb y el paquete Moongose. Se ha configurado el
    paquete en index.js de forma que cuando la conexión a mongodb es exitosa
    se inicie el servidor.

--- controllers/favorito.js
+++ controllers/favorito.js
@@ -8,36 +8,29 @@ function prueba(req, res){
 };
 
 function getFavorito(req,res){
-	//Obtenemos el id pasado como parámetro
 	var favoritoId = req.params.id;
-
-	//Devolvemos un status 200 y el Id
 	res.status(200).send({data: favoritoId})
 }
 
 function getFavoritos(req,res){
-	
+
 }
 
 function saveFavorito(req,res){
-	//Variable en que almacenamos parámetros POST
 	var params = req.body;
 	res.status(200).send({favorito: params});
 }
 
 function updateFavorito(req,res){
-	//Variable en que almacenamos parámetros POST
 	var params = req.body;
 	res.status(200).send({update:true, favorito: params});
 }
 
 function deleteFavorito(req,res){
-	//Variable en que almacenamos parámetros POST
 	var params = req.body;
 	res.status(200).send({delete:true, favorito: params});
 }
 
-//Exportamos nuestras funciones
 module.exports = {
 	prueba,
 	getFavorito,
--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 'use strict'
 
+//Cargamos mongoose
+var mongoose = require('mongoose');
 var app = require('./app');
 var port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-	console.log(`API REST FAVORITOS funcionando en http://localhost:${port}`);
+//Y lo configuramos pasando como parámetros la uri de la DB y una función callback en la que iniciaremos el servidor
+mongoose.connect('mongodb://localhost:27017/api_rest_favoritos', (err,res) =>{
+	//Si existe un error lo capturamos, si no, lanzamos el servidor
+	if(err){
+		//Lanzamos excepción
+		throw err;
+	}else{
+		console.log('Conexión a MongoSB realizada con éxito.')
+		app.listen(port, () => {
+			console.log(`API REST FAVORITOS funcionando en http://localhost:${port}`);
+		});
+	}
 });
+
+
